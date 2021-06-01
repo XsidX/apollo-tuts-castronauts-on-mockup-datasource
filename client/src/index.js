f909d6ac4d11be532c370735adda221ef6853ab7
iamsid <developer.sid@outlook.com>
2021-06-01 17:31:43 +0300

    Apollo Client setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  // options go here
+
+  //to specify the location of our GraphQL server.
+  uri: 'http://localhost:4000', 
+
+  //in-memory cache...store and reuse query results 
+  cache: new InMemoryCache(),
+});
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
