74322b001e7f565855af178e4421d42d005aeb30
iamsid <developer.sid@outlook.com>
2021-06-01 15:02:18 +0300

    Building Schema

---
+++ client/src/schema.js
@@ -0,0 +1,28 @@
+const {gql} = require('apollo-server');
+
+
+const typeDefs = gql`
+  # Schema definitions go here
+type Query {
+  "Get tracks array for homepage grid"
+  tracksForHome: [Track!]!
+}
+  "A track is a group of Modules that teaches about a specific topic"
+type Track {
+
+  id: ID!
+  title: String!
+  author: Author!
+  thumbnail: String
+  length: Int
+  modulesCount: Int
+}
+"Author of a complete Track or a Module"
+type Author {
+    id: ID!
+  name: String!
+  photo: String
+}
+
+`;
+module.exports = typeDefs;
